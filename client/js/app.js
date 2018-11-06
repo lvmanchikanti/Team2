1b07105c585c6feef73b62c86a19c2427ced98d2
Lahari Manchikanti <lahari@Laharis-MacBook-Pro.local>
2018-11-06 16:06:08 -0500

    beginning of ngroute

--- client/js/app.js
+++ client/js/app.js
@@ -2,4 +2,25 @@
 angular.module('user', []);
 
 /* register the application and inject all the necessary dependencies */
-var app = angular.module('ufxApp', ['user']);
+// var app = angular.module('ufxApp', ['user', require('angular-route')]);
+var app = angular.module('ufxApp', ['user', 'ngRoute']);
+
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: '/',
+            controller: 
+        })
+		.when('/login', {
+			templateUrl: 'js/html/login.html',
+			controller: 'loginController'
+		})
+		.when('/view2', {
+			templateUrl: 'view2.html',
+			controller: 'SecondController'
+		})
+		// .otherwise({
+		// 	redirectTo: '/view1'
+		// });
+});
