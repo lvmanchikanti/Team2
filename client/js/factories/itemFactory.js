7b350a79510167825a36a561b5f46dbfbdc93369
Kassandra <kassallaixp@gmail.com>
2018-11-07 15:11:07 -0500

    Listing Commit 2

--- client/js/factories/itemFactory.js
+++ client/js/factories/itemFactory.js
@@ -1,24 +1,24 @@
 angular.module('listings', []).factory('Listings', function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/buying');
+        return $http.get('http://localhost:3000/buying');
       },
 
       createBuying: function(listing) {
-        return $http.post('http://localhost:8080/buying', listing);
+        return $http.post('http://localhost:3000/buying', listing);
       },
 
       getSelling: function() {
-        return $http.get('http://localhost:8080/selling');
+        return $http.get('http://localhost:3000/selling');
       },
 
       getCurrentSelling: function(id) {
         console.log("here1");
-        return $http.get('http://localhost:8080/selling/:id', id);
+        return $http.get('http://localhost:3000/selling/:id', id);
       },
 
       createSelling: function(listing) {
-        return $http.post('http://localhost:8080/selling', listing);
+        return $http.post('http://localhost:3000/selling', listing);
       },
 
     };
--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-//This file holds any configuration variables we may need 
+//This file holds any configuration variables we may need
 //'config.js' is ignored by git to protect sensitive information, such as your database's username and password
 //copy this file's contents to another file 'config.js' and store your MongoLab uri there
 
@@ -6,7 +6,7 @@ module.exports = {
   db: {
     uri: 'mongodb://team2user:glackr1@ds215822.mlab.com:15822/ufx', //place the URI of your mongo database here.
   },
-  port: process.env.PORT || 8080
+  port: process.env.PORT || 3000
 };
 
 /* Now go to the JSONtoMongo.js file and include this file as a variable named 'config' with a require() */
